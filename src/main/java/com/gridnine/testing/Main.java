d9cbc3efe0117468be61a01d8445be26ece5dc02
Kawaiiskii <antonzhadnov@gmail.com>
2023-08-31 16:09:24 +0300

    implement Main

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,7 +1,29 @@
 package com.gridnine.testing;
 
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Flight> flights = FlightBuilder.createFlights();
+
+        ArrivalMoreDepartureCondition arrivalMoreDepartureCondition = new ArrivalMoreDepartureCondition();
+        DepartureBeforeCurrentTimeCondition departureBeforeCurrentTimeCondition = new DepartureBeforeCurrentTimeCondition();
+        TimeOnEarthMoreSomeHoursCondition timeOnEarthMoreSomeHoursCondition= new TimeOnEarthMoreSomeHoursCondition(2);
+
+        FlyingFilterService flyingFilterService = new FlyingFilterService();
+
+        System.out.println(flyingFilterService.filter(flights, arrivalMoreDepartureCondition));
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println(flyingFilterService.filter(flights, departureBeforeCurrentTimeCondition));
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println(flyingFilterService.filter(flights, timeOnEarthMoreSomeHoursCondition));
+
+
+        System.out.println(flyingFilterService.filter(null, timeOnEarthMoreSomeHoursCondition));
+
+
+
 
     }
 }
